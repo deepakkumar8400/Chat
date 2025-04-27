66697250ebdbb4f9219a49f3e3e9e5e78b8f1fbf
Deep3713 <145797364+deepakkumar8400@users.noreply.github.com>
2025-04-27 12:36:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'https://frontendchat-q13n.onrender.com/login',
+    origin:'https://frontendchat-q13n.onrender.com',
     credentials:true
 };
 app.use(cors(corsOption)); 
